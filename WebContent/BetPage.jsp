d2364b210f73166cf6e5edc576906027937fb480
ccutmt <christophercutajar@gmail.com>
2014-11-07 15:38:53 +0100

    Bet Page and validation

---
+++ WebContent/BetPage.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<link rel="stylesheet" type="text/css" href="register.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="betvalidation.js"></script>
+
+<title>Bet Page</title>
+
+</head>
+<body>
+
+	<div id="register-form">
+		<div class="fieldset">
+
+			<legend>Bets</legend>
+			<form id="bet_form" action="" method="post">
+
+				<div class="row">
+					<label for="risk"><br></label> Risk Level <br><br> Low
+					<input type="radio" name="risk_lvl" id="risk_lvl" value="0" checked/> Medium
+					<input type="radio" name="risk_lvl" id="risk_lvl" value="1"/> High 
+					<input type="radio" name="risk_lvl" id="risk_lvl" value="2"/> 
+				</div>
+
+				<div class="row">
+					<label for="BetAmount"><br>Bet Amount </label> 
+					<input type="number" placeholder="Bet Amount" name="bet_amt" id="bet_amt"/>
+					<span class="error">Error in the Bet Amount field </span>
+				</div>
+								
+				<div id="place_bet">
+					<input type="submit" value="Place Bet"></input>
+				</div>
+			</form>
+			
+		</div>
+	</div>
+</body>
+</html>
---
+++ WebContent/betvalidation.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+	// Bet Amount real-time validation
+	$("#bet_amt").blur(function() {
+		if (validateBetAmount($("#bet_amt").val())) {
+			$("#bet_amt").removeClass('invalid');
+			$("#bet_amt").addClass('valid');
+		} else {
+			$("#bet_amt").removeClass('valid');
+			$("#bet_amt").addClass('invalid');
+		}
+	});
+
+	// Register button, checks that all fields are correct before submitting the
+	// data
+	$("#place_bet").click(function(event) {
+		var form_data = $("#bet_form").serializeArray();
+		var error_free = true;
+		for ( var input in form_data) {
+			var element = $("#" + form_data[input]['name']);
+			var valid = element.hasClass('valid');
+			var error_element = $("span", element.parent());
+			if (!valid) {
+				error_element.removeClass('error').addClass('error_show');
+				error_free = false;
+			} else {
+				error_element.removeClass('error_show').addClass('error');
+			}
+		}
+
+		if (!error_free) {
+			event.preventDefault();
+		} else {
+			alert('No errors: Form will be submitted');
+		}
+	});
+});
+
+// The below function will check the amount if it contains any characters which are not integers
+function validateBetAmount(n) {
+	var nan = false;
+	if(n == 0){
+		return false;
+	}
+	else {
+		for (i = 0; i < n.length; i++) {
+			if (isNaN(n.charAt(i)) == true) {
+				nan = true;
+				break;
+			} else{
+				nan = false;
+			}
+		}
+	}
+
+	if (nan == true)
+		return false;
+	else
+		return true;
+}
