2266313d1e548cbef68ea84627f70765c7637424
Afriggieri1 <andreasfriggieri@gmail.com>
2014-12-19 16:21:08 +0100

    Insert into bets table, still has a bug
    validation on risk level and amount, tests for betting methods,
    generating ID for bet. After solving this bug we need to limit the
    maximum free user bets to 3

--- src/Servlets/BetServlet.java
+++ src/Servlets/BetServlet.java
@@ -2,6 +2,10 @@ package Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,48 +13,130 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Database.DBConnection;
+
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+
 /**
  * Servlet implementation class BetServlet
  */
 @WebServlet("/BetServlet")
 public class BetServlet extends HttpServlet {
+	long CurrentBetID;
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BetServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Placing bet...");
-		
-		PrintWriter writer = response.getWriter();
-		writer.println(request.getParameter("username"));
-		writer.println(request.getParameter("risk_lvl"));
-		writer.println(request.getParameter("bet_amt"));
+	public BetServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Placing bet...");
+		try {
+			DBConnection account_type = new DBConnection(
+					"SELECT account FROM players WHERE Username = \""
+							+ UsernameValidation(request
+									.getParameter("username")) + "\";");
+
+			DBConnection total_bets = new DBConnection(
+					"SELECT sum(amount) FROM bets WHERE Username = \""
+							+ UsernameValidation(request
+									.getParameter("username")) + "\";");
+
+			new DBConnection(
+					"INSERT INTO bets (username, Bet_ID, RiskLevel, amount) VALUES (\""
+							+ UsernameValidation(request
+									.getParameter("username"))
+							+ "\", \""
+							+ GenerateBetID()
+							+ "\", \""
+							+ ValidateRiskLevel(Integer.parseInt(account_type
+									.getResults().get(0)), Integer
+									.parseInt(request.getParameter("risk_lvl")))
+							+ "\", \""
+							+ ValidateBetAmount(Integer.parseInt(account_type
+									.getResults().get(0)), Integer
+									.parseInt(request.getParameter("bet_amt")),
+									getTotalBetAmount(total_bets.getResults()))
+							+ "\", \"1\");");
+			
+			PrintWriter writer = response.getWriter();
+			writer.println("Bet Placed Successfully");
+			writer.println(request.getParameter("username"));
+			writer.println(request.getParameter("risk_lvl"));
+			writer.println(request.getParameter("bet_amt"));
+
+		} catch (MySQLIntegrityConstraintViolationException primarykey_violation) {
+			System.out.println("There exists a bet with the same bet ID!");
+		} catch (SQLException se) {
+			System.out.println("Invalid Bet Parameters");
+			se.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-	
-	public String ValidateRiskLevel(){
-		return "";
-	}
-	
-	public int ValidateBetAmount(){
-		return 0;
+
+	public String UsernameValidation(String username) throws SQLException {
+		Pattern p = Pattern.compile("^[A-Za-z0-9]+$");
+		Matcher m = p.matcher(username);
+		boolean b = m.find();
+		if (b == true && username.length() >= 6) {
+			return username;
+		} else {
+			throw new SQLException();
+		}
 	}
 
+	public int ValidateRiskLevel(int account_type, int risk_level)
+			throws SQLException {
+		if (((account_type == 0) && (risk_level == 0))
+				|| ((account_type == 1) && (risk_level >= 0) && (risk_level < 3))) {
+			return risk_level;
+		} else
+			throw new SQLException();
+	}
+
+	public int ValidateBetAmount(int account_type, int bet_amount,
+			int total_bets) throws SQLException {
+		if (bet_amount > 0
+				&& total_bets >= 0
+				&& (((account_type == 0) && (bet_amount <= 5) && (total_bets
+						+ bet_amount <= 15)) || ((account_type == 1) && ((total_bets + bet_amount) <= 5000)))) {
+			return bet_amount;
+		} else
+			throw new SQLException();
+	}
+
+	public long GenerateBetID() {
+		try {
+			BetServlet.this.CurrentBetID++;
+		} catch (Exception e) {
+			BetServlet.this.CurrentBetID = 0;
+		}
+
+		return CurrentBetID;
+	}
+
+	public int getTotalBetAmount(ArrayList<String> arraylist) {
+		if (arraylist.get(0) == null) {
+			return 0;
+		} else
+			return Integer.parseInt(arraylist.get(0));
+	}
 }
-
-
---
+++ src/tests/BetTests.java
@@ -0,0 +1,279 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Servlets.BetServlet;
+
+public class BetTests {
+	private BetServlet betservlet;
+
+	@Before
+	public void setUp() throws Exception {
+		betservlet = new BetServlet();
+	}
+
+	@Test
+	public void ValidateUsernameTest() throws SQLException {
+
+		assertEquals("UsernameTesting",
+				betservlet.UsernameValidation("UsernameTesting"));
+	}
+
+	@Test
+	public void ValidateUsernameNumberTest() throws SQLException {
+
+		assertEquals("UsernameTesting123",
+				betservlet.UsernameValidation("UsernameTesting123"));
+	}
+
+	@Test
+	public void ShortUsernameTest() {
+		try {
+			betservlet.UsernameValidation("chris");
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+
+	@Test
+	public void UsernameSpecialCharTest() {
+		try {
+			betservlet.UsernameValidation("UsernameTesting!!");
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+
+	@Test
+	public void ValidateRiskLevelFreeLow() throws SQLException {
+		assertEquals(0, betservlet.ValidateRiskLevel(0, 0));
+	}
+	
+	@Test
+	public void ValidateRiskLevelFreeMed() throws SQLException{
+		try {
+			betservlet.ValidateRiskLevel(0, 1);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateRiskLevelFreeHigh() throws SQLException{
+		try {
+			betservlet.ValidateRiskLevel(0, 2);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateRiskLevelPremiumLow() throws SQLException {
+		assertEquals(0, betservlet.ValidateRiskLevel(1, 0));
+	}
+	
+	@Test
+	public void ValidateRiskLevelPremiumMed() throws SQLException {
+		assertEquals(1, betservlet.ValidateRiskLevel(1, 1));
+	}
+	
+	@Test
+	public void ValidateRiskLevelPremiumHigh() throws SQLException {
+		assertEquals(2, betservlet.ValidateRiskLevel(1, 2));
+	}
+	
+	@Test
+	public void ValidateRiskLevelOutofBounds1() throws SQLException{
+		//setting account type to 2 should fail
+		try {
+			betservlet.ValidateRiskLevel(2, 2);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateRiskLevelOutofBounds2() throws SQLException{
+		//setting risk level to 3 should fail
+		try {
+			betservlet.ValidateRiskLevel(1, 3);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateBetAmountFreeZeroBet() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(0, 0, 0);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateBetAmountFreeOverLimit() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(0, 10, 0);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidatBetAmountFreeValid1() throws SQLException {
+		assertEquals(5, betservlet.ValidateBetAmount(0, 5, 0));
+	}
+	
+	@Test
+	public void ValidatBetAmountFreeValid2() throws SQLException {
+		assertEquals(3, betservlet.ValidateBetAmount(0, 3, 0));
+	}
+	
+	@Test
+	public void ValidatBetAmountFreeValid3() throws SQLException {
+		assertEquals(1, betservlet.ValidateBetAmount(0, 1, 0));
+	}
+	
+	@Test
+	public void ValidateBetAmountFreeOverTotal() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(0, 5, 15);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidatBetAmountFreeMaxCumulatuveBets() throws SQLException {
+		assertEquals(5, betservlet.ValidateBetAmount(0, 5, 10));
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumZeroBet() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(1, 0, 0);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumOverLimit() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(1, 5001, 0);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumValidBet1() throws SQLException {
+		assertEquals(1, betservlet.ValidateBetAmount(1, 1, 0));
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumValidBet2() throws SQLException {
+		assertEquals(2000, betservlet.ValidateBetAmount(1, 2000, 0));
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumValidBet3() throws SQLException {
+		assertEquals(5000, betservlet.ValidateBetAmount(1, 5000, 0));
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumMaxCumulativeBets() throws SQLException {
+		assertEquals(2000, betservlet.ValidateBetAmount(1, 2000, 3000));
+	}
+	
+	@Test
+	public void ValidateBetAmountPremiumOverTotal() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(1, 2001, 3000);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateBetAmountAccountOutofBounds() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(2, 0, 0);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void ValidateBetAmountTotalOutofBounds() throws SQLException {
+		try {
+			betservlet.ValidateBetAmount(1, 0, -2000);
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+	
+	@Test
+	public void GenerateBetID1() throws SQLException {
+		assertEquals(1, betservlet.GenerateBetID());
+	}
+	
+	@Test
+	public void GenerateBetID2() throws SQLException {
+		assertEquals(2, betservlet.GenerateBetID());
+	}
+	
+	@Test
+	public void GenerateBetID3() throws SQLException {
+		assertEquals(3, betservlet.GenerateBetID());
+	}
+}
