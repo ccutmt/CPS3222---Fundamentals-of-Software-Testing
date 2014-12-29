c6e8c8e8c3364303dda01c3a3cf502bb0ae72afa
ccutmt <christophercutajar@gmail.com>
2014-12-29 15:26:11 +0100

    Username Tests(BettingFunctions)

---
+++ src/tests/BettingFunctionsTests.java
@@ -0,0 +1,55 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Functions.BettingFunctions;
+
+public class BettingFunctionsTests {
+	private BettingFunctions betfunctions;
+
+	@Before
+	public void setUp() throws Exception {
+		betfunctions = new BettingFunctions();
+	}
+	
+	@Test
+	public void ValidateUsernameTest() throws SQLException {
+		assertEquals("UsernameTesting", betfunctions.UsernameValidation("UsernameTesting"));
+	}
+
+	@Test
+	public void ValidateUsernameNumberTest() throws SQLException {
+		assertEquals("UsernameTesting123", betfunctions.UsernameValidation("UsernameTesting123"));
+	}
+
+	@Test
+	public void ShortUsernameTest() {
+		try {
+			betfunctions.UsernameValidation("chris");
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+
+	@Test
+	public void UsernameSpecialCharTest() {
+		try {
+			betfunctions.UsernameValidation("UsernameTesting!!");
+
+			fail("expected SQLException");
+
+		} catch (SQLException e) {
+			// ignore, this exception is expected.
+		}
+	}
+
+}
