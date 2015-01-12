92f1a44b227996ecf4b97899c9bdc93bd191e469
ccutmt <christophercutajar@gmail.com>
2015-01-12 01:59:38 +0100

    Revert "Deleted comments"
    This reverts commit 20d4cf3b5198bee05557239086af239d715d714a.

--- WebContent/betvalidation.js
+++ WebContent/betvalidation.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
 
-	// Bet Amount real-time validation	
+	// Bet Amount real-time validation
+	/*$("#bet_amt").blur(function() {
+		if (validateBetAmount($("#bet_amt").val())) {
+			$("#bet_amt").removeClass('invalid');
+			$("#bet_amt").addClass('valid');
+		} else {
+			$("#bet_amt").removeClass('valid');
+			$("#bet_amt").addClass('invalid');
+		}
+	});*/
+	
 	$("#bet_amt").blur(function() {
 		var element = $("#bet_amt");
 		if (validateBetAmount(element.val())) {
