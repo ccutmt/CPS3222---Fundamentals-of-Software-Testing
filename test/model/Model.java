30a1431996864aecf208f42fdd048dbe6f211a28
Afriggieri1 <andreasfriggieri@gmail.com>
2015-01-12 14:31:41 +0100

    Model fix v2

--- test/model/Model.java
+++ test/model/Model.java
@@ -7,9 +7,16 @@ import java.util.Random;
 
 import net.sourceforge.czt.modeljunit.Action;
 import net.sourceforge.czt.modeljunit.AllRoundTester;
+import net.sourceforge.czt.modeljunit.RandomTester;
+import net.sourceforge.czt.modeljunit.LookaheadTester;
+import net.sourceforge.czt.modeljunit.GreedyTester;
 import net.sourceforge.czt.modeljunit.FsmModel;
 import net.sourceforge.czt.modeljunit.Tester;
 import net.sourceforge.czt.modeljunit.VerboseListener;
+import net.sourceforge.czt.modeljunit.coverage.ActionCoverage;
+import net.sourceforge.czt.modeljunit.coverage.StateCoverage;
+import net.sourceforge.czt.modeljunit.coverage.TransitionCoverage;
+import net.sourceforge.czt.modeljunit.coverage.TransitionPairCoverage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -164,12 +171,10 @@ public class Model implements FsmModel, Runnable {
 	@Action
 	public void proceedToLogin() {
 		long timeBefore = System.currentTimeMillis();
-		;
 		browser.findElement(By.id("login")).click();
 		assertEquals("http://localhost:8080/SoftwareTesting/Login.jsp",
 				browser.getCurrentUrl());
 		long timeAfter = System.currentTimeMillis();
-		;
 		responseTimes.add(timeAfter - timeBefore);
 	}
 
@@ -178,7 +183,7 @@ public class Model implements FsmModel, Runnable {
 		// 25% chance to be invalid if less than 3 attempted logins, else
 		// timeout state
 		if (prob <= 0.25 && getState().equals(States.LoginPage)
-				&& this.attempted_logins < 3) {
+				&& this.attempted_logins <= 3) {
 			return true;
 		} else
 			return false;
@@ -191,27 +196,40 @@ public class Model implements FsmModel, Runnable {
 		log_form.fillForm(this.username, this.password + " ");
 		this.attempted_logins++;
 		long timeBefore = System.currentTimeMillis();
-		;
 		log_form.submitForm();
 		assertEquals(
 				"http://localhost:8080/SoftwareTesting/Pages/LoginFailed.html",
 				browser.getCurrentUrl());
 		long timeAfter = System.currentTimeMillis();
-		;
 		responseTimes.add(timeAfter - timeBefore);
 	}
 
+	public boolean proceedToLoginTimeoutGuard() {
+		if (getState().equals(States.LoginPage) && this.attempted_logins > 3) {
+			return true;
+		} else
+			return false;
+
+	}
+
+	@Action
+	public void proceedToLoginTimeout() {
+		assertEquals(
+				"http://localhost:8080/SoftwareTesting/Pages/LoginTimeout.html",
+				browser.getCurrentUrl());
+		//this.attempted_logins = 0;
+	}
+
 	public boolean proceedToBetGuard() {
-		if (getState().equals(States.LoginPage)) {
+		double probFailLogin = Math.random();
+		if (getState().equals(States.LoginPage) && probFailLogin > 0.25) {
 			return true;
 		} else if (getState().equals(States.Bet_Amount_Error)
 				|| getState().equals(States.Bet_Cumulative_Error)
 				|| getState().equals(States.Bet_Exceeded3Bets_Error)
 				|| getState().equals(States.Bet_Risk_Error)
 				|| getState().equals(States.Bet_Successful)) {
-			// if state is an error page, user has to click on return button to
 			// visit betting state
-			browser.findElement(By.id("bet")).click();
 			return true;
 		} else
 			return false;
@@ -219,22 +237,30 @@ public class Model implements FsmModel, Runnable {
 
 	@Action
 	public void proceedToBet() {
-		FillLogin log_form = new FillLogin(browser);
-		log_form.fillForm(this.username, this.password);
-		long timeBefore = System.currentTimeMillis();
-		;
-		log_form.submitForm();
+		if (getState().equals(States.LoginPage)) {
+			FillLogin log_form = new FillLogin(browser);
+			log_form.fillForm(this.username, this.password);
+			long timeBefore = System.currentTimeMillis();
+			log_form.submitForm();
+			long timeAfter = System.currentTimeMillis();
+			responseTimes.add(timeAfter - timeBefore);
+		} else {
+			long timeBefore = System.currentTimeMillis();
+			browser.findElement(By.id("bet")).click();
+			long timeAfter = System.currentTimeMillis();
+			responseTimes.add(timeAfter - timeBefore);
+		}
+
 		assertEquals("http://localhost:8080/SoftwareTesting/BetPage.jsp",
 				browser.getCurrentUrl());
-		long timeAfter = System.currentTimeMillis();
-		;
-		responseTimes.add(timeAfter - timeBefore);
 	}
 
 	public boolean proceedToBetSuccessGuard() {
+		double probNotLogout = Math.random();
 		double probInvalidBetAmount = Math.random();
-		if ((getState().equals(States.BettingPage))
-				&& ((this.acount_type.compareTo("premium") == 0) || (probInvalidBetAmount > (1 / 6)))) {
+		if (probNotLogout > 0.5
+				&& ((getState().equals(States.BettingPage)) && ((this.acount_type
+						.compareTo("premium") == 0) || (probInvalidBetAmount > (1 / 6))))) {
 			// free user can also bet 6 euro, which is invalid
 			return true;
 		} else
@@ -249,45 +275,72 @@ public class Model implements FsmModel, Runnable {
 			// 1 to 5 since 6euro is invalid and catered for in the Guard
 			int randomAmount = random.nextInt(5) + 1;
 			bet_form.fillForm(randomAmount + "", "low");
+			//this.cumulative_bets += randomAmount;
 		} else {
 			// 100 to 2000 euro
 			int randomAmount = random.nextInt(2000) + 100;
 			bet_form.fillForm(randomAmount + "", "medium");
+			//this.cumulative_bets += randomAmount;
 		}
 		long timeBefore = System.currentTimeMillis();
-		;
 		bet_form.submitForm();
 		assertEquals(
 				"http://localhost:8080/SoftwareTesting/Pages/BetSuccess.html",
 				browser.getCurrentUrl());
 		long timeAfter = System.currentTimeMillis();
-		;
+		responseTimes.add(timeAfter - timeBefore);
+		this.atLeastOneBet = true;
+	}
+
+	public boolean proceedToBetAmountErrorGuard() {
+		double probInvalidBetAmount = Math.random();
+		if (getState().equals(States.BettingPage)
+				&& probInvalidBetAmount <= 1 / 6
+				&& this.acount_type.compareTo("free") == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Action
+	public void proceedToBetAmountError() {
+		FillBet bet_form = new FillBet(browser);
+		bet_form.fillForm(6 + "", "low");
+
+		long timeBefore = System.currentTimeMillis();
+		bet_form.submitForm();
+		assertEquals(
+				"http://localhost:8080/SoftwareTesting/Pages/InvalidBetAmount.html",
+				browser.getCurrentUrl());
+		long timeAfter = System.currentTimeMillis();
 		responseTimes.add(timeAfter - timeBefore);
 		this.atLeastOneBet = true;
 	}
 
 	public boolean logoutGuard() {
-		double ran = Math.random();
+		// double ran = Math.random();
 		// user has to place at least 1 bet to logout
-		return (ran <= 0.5 && this.atLeastOneBet && getState().equals(
-				States.BettingPage));
+		/* ran <= 0.5 && */if (this.atLeastOneBet
+				&& getState().equals(States.BettingPage)) {
+			return true;
+		} else
+			return false;
 	}
 
 	@Action
 	public void logout() {
 		long timeBefore = System.currentTimeMillis();
-		;
 		browser.findElement(By.id("logout")).click();
 		assertEquals("http://localhost:8080/SoftwareTesting/HomePage.jsp",
 				browser.getCurrentUrl());
 		long timeAfter = System.currentTimeMillis();
-		;
 		responseTimes.add(timeAfter - timeBefore);
 	}
 
 	public String generateUsername() {
 		Random rand = new Random();
-		int randomNum = rand.nextInt((999999999)) + 1;
+		int randomNum = rand.nextInt((9999999)) + 1;
 		return "TestUser" + randomNum;
 	}
 
@@ -296,9 +349,24 @@ public class Model implements FsmModel, Runnable {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		browser = new ChromeDriver();
 		Tester t = new AllRoundTester(this);
+		// LookaheadTester t = new LookaheadTester(this);
+		// t.setDepth(1);
+		t.addCoverageMetric(new TransitionCoverage());
+		t.addCoverageMetric(new StateCoverage());
+		t.addCoverageMetric(new ActionCoverage());
+		t.addCoverageMetric(new TransitionPairCoverage());
 		t.addListener(new VerboseListener());
-		t.generate(25);
+		t.generate(10);
 		t.buildGraph();
 		browser.close();
+		t.printCoverage();
 	}
+
+	/*
+	 * @Override public void run() {
+	 * System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	 * browser = new ChromeDriver(); Tester t = new AllRoundTester(this);
+	 * t.addListener(new VerboseListener()); t.generate(25); t.buildGraph();
+	 * browser.close(); }
+	 */
 }
